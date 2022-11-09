97a1adec49b13e79b46bc35c1c0964be65ecab8e
yesolll <9595kys@gmail.com>
2022-11-09 22:56:56 +0900

    Q131705 삼총사

---
+++ src/com/yesolll/programmers/lv1/Trio.java
@@ -0,0 +1,22 @@
+package com.yesolll.programmers.lv1;
+
+public class Trio {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) { // 무조건 뒤로만 더하기
+                for (int k = j+1; k < numbers.length; k++) {
+                    if (numbers[i]+numbers[j]+numbers[k] == 0)
+                    answer ++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Trio q = new Trio();
+        int output = q.solution(new int[] {-3, -2, -1, 0, 1, 2, 3});
+        System.out.println(output);
+    }
+}
