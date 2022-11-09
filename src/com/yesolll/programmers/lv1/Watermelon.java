fd116c7ab5031a4e5598a350095926421db02277
yesolll <9595kys@gmail.com>
2022-11-09 21:25:21 +0900

    Q12922 수박수박수

---
+++ src/com/yesolll/programmers/lv1/Watermelon.java
@@ -0,0 +1,12 @@
+package com.yesolll.programmers.lv1;
+
+public class Watermelon {
+    public String solution(int n) {
+        String answer = "";
+        
+        answer += "수박".repeat(n / 2);
+        if (n % 2 == 1) answer += "수";
+        
+        return answer;
+    }
+}
