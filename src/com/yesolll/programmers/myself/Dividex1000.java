334f38a27f21ac76b8deb9891ef10a9234d945ff
yesolll <9595kys@gmail.com>
2022-11-12 23:24:06 +0900

    Q120806 두 수의 나눗셈

---
+++ src/com/yesolll/programmers/myself/Dividex1000.java
@@ -0,0 +1,18 @@
+package com.yesolll.programmers.myself;
+
+public class Dividex1000 {
+    public static int solution(int num1, int num2) {
+
+        int answer = 0;
+        double div = num1 / (double) num2;
+        answer = (int) Math.floor(div * 1000);
+        return answer;
+        
+    }
+
+    public static void main(String[] args) {
+        int output = solution(1, 16);
+        System.out.println(output);
+    }
+    
+}
