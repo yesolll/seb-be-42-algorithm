0f01fcaa059181007fecc0cf487ca7e4c2cb77c4
yesolll <9595kys@gmail.com>
2022-11-15 23:22:14 +0900

    String replace example

---
+++ src/com/yesolll/programmers/study/ReplaceAll.java
@@ -0,0 +1,28 @@
+package com.yesolll.programmers.study;
+
+public class ReplaceAll {
+    public static void main(String[] args) {
+        
+    String my_string = "DDorimeo blog";
+    System.out.println(my_string);
+
+    String letter = "o";
+    my_string.replace(letter, "");
+    
+    System.out.println(my_string);
+    }
+
+    // public static void main(String[] args) {
+        
+    //     String my_string = "DDorimeo blog";
+    //     System.out.println(my_string);
+    
+    //     String letter = "o";
+    
+    //     my_string = my_string.replaceAll("[Dbg]", "");
+
+        
+    //     System.out.println(my_string);
+    // }
+    
+}
