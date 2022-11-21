382560e3d992c4eb20f8c3a74c8868d193dd44a8
yesolll <9595kys@gmail.com>
2022-11-21 21:45:26 +0900

    Q42889 실패율

--- src/com/yesolll/programmers/day5/FailureRate.java
+++ src/com/yesolll/programmers/day5/FailureRate.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class FailureRate {
     public static int[] solution(int N, int[] stages) {
-        // [2, 1, 2, 6, 2, 4, 3, 3]
+        // ex) N=5, stages = [2, 1, 2, 6, 2, 4, 3, 3]
 
         List<Integer> stageList = Arrays.stream(stages).boxed().collect(Collectors.toList());
         int ChallengerCnt = stages.length;
@@ -18,7 +18,7 @@ public class FailureRate {
         // <스테이지, 실패율>
         Map<Integer, Double> map = new HashMap<>();
 
-        /* 1. 스테이지 별 실페율 구해서 map에 넣어주기 */
+        /* 1. 스테이지 별 실패율 구해서 map에 넣어주기 */
         // 스테이지 1, 2, 3, ..., N까지 반복하면서
         for (int i = 1; i <= N; i++) {
             // == 스테이지 i에 아직 클리어하지 못 한 유저가 몇 명인지 세어준다.
@@ -28,16 +28,16 @@ public class FailureRate {
             map.put(i, failure == 0 ? 0 : failure/(double)ChallengerCnt);
 
             // 현재 스테이지 도전자 수    == 총 도전자 수 (ChallengerCnt) - 이전 스테이지를 클리어하지 못한 사람 수 (failure)
-            // ex) 2번 스테이지 도전자 수 == 총 도전자 수(8)              - 1번 스테이지 클리어하지 못한 사람(1) => 7명
-            //     3번 스테이지 도전자 수 == 2번 스테이지 도전자 수(7)     - 2번 스테이지 클리어하지 못한 사람(3) => 4명
+            // ex) 2번 스테이지 도전자 수 ==        총 도전자 수(8)       - 1번 스테이지 클리어하지 못한 사람(1) => 7명
+            //     3번 스테이지 도전자 수 == 2번 스테이지 도전자 수(7)    - 2번 스테이지 클리어하지 못한 사람(3) => 4명
             ChallengerCnt = ChallengerCnt - failure;
         }
 
         /* 맵 결과 확인 */
-        // for(Entry<Integer, Double> entry : map.entrySet()) {
-        //     System.out.print("key: " +  entry.getKey() + " ");
-        //     System.out.println("value " + entry.getValue());
-        // }
+        for(Entry<Integer, Double> entry : map.entrySet()) {
+            System.out.print("key: " +  entry.getKey() + " ");
+            System.out.println("value " + entry.getValue());
+        }
 
         /* 2. 실패율을 기준으로 정렬해주기 */
         // 최종적으로 만들어진 {스테이지 K = 실패율 V} map을 실패율(V) 기준 내림차순으로 정렬                   (문제 조건)
@@ -54,7 +54,7 @@ public class FailureRate {
     }
 
     public static void main(String[] args) {
-        // int[] output = solution(5, new int[] {2, 1, 2, 6, 2, 4, 3, 3});
+        int[] output = solution(5, new int[] {2, 1, 2, 6, 2, 4, 3, 3});
         // for (int i : output) {
         //     System.out.println(i);
         // }
