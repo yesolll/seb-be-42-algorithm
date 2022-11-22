9f3aadcfd3dc5eff8c5e0a0615bf1501c1f459b3
yesolll <9595kys@gmail.com>
2022-11-22 22:33:04 +0900

    Q120906 자릿수 더하기

---
+++ src/com/yesolll/programmers/myself/lv0/SumInt.java
@@ -0,0 +1,18 @@
+package com.yesolll.programmers.myself.lv0;
+
+import java.util.stream.Stream;
+
+public class SumInt {
+    public int solution(int n) {
+        // int answer = 0;
+        // String[] str = (n + "").split("");
+        // answer = Stream.of(str).mapToInt(Integer::parseInt).sum();
+        // return answer;        
+        int answer = 0;
+        String str = (n + "");
+        for (int i = 0; i < str.length(); i++) {
+            answer += (str.charAt(i) - '0');
+        }
+        return answer;
+    }
+}
