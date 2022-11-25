2fe06d944c5a79ca7731c4c0266a88b830c145d3
yesolll <9595kys@gmail.com>
2022-11-26 01:49:46 +0900

    Add comments

--- src/com/yesolll/programmers/day7/ShiftString.java
+++ src/com/yesolll/programmers/day7/ShiftString.java
@@ -18,10 +18,11 @@ public class ShiftString {
     public static int solution(String A, String B) {
 
         String shiftedA = shift(A);
-        int shiftCnt = 1;
+        int shiftCnt = 1; // 한 번 밀어주고 시작 (while 조건 때문에)
         
-        if(A.equals(B)) return 0;
-        while (!shiftedA.equals(A)) {
+        if(A.equals(B)) return 0; // 같은 문자열이 들어오면 0번!
+
+        while (!shiftedA.equals(A)) { // 자기 자신으로 돌아올 때까지 밀어주기
             if (shiftedA.equals(B)) return shiftCnt;
             shiftedA = shift(shiftedA);
             shiftCnt++;
@@ -30,11 +31,13 @@ public class ShiftString {
 
     }
 
+    // >> 오른쪽으로 한 칸씩 밀어주는 작업
     private static String shift(String A) {
+        // ex) A = Algorith"m" -----> "m"Algorith
         return A.charAt(A.length()-1) + A.substring(0, A.length()-1);
     }
 
     public static void main(String[] args) {
-        System.out.println(solution("aaa", "aaa"));
+        System.out.println(solution("Algorithm", "hmAlgorit"));
     }
 }
--- src/com/yesolll/programmers/day7/Uniform.java
+++ src/com/yesolll/programmers/day7/Uniform.java
@@ -21,25 +21,26 @@ public class Uniform {
     public static int solution(int n, int[] lost, int[] reserve) {
 
         // 1. n + 2 크기의 체육복 수 배열 만들기 
-        int[] uniforms = new int[n+2];
+        int[] uniforms = new int[n+2]; // 맨 앞, 맨 뒤는 index 에러 처리 없이 쉽게 비교하려고... 값은 0으로
+
         for (int i = 0; i < uniforms.length; i++) {
-            uniforms[i] = 1;
+            uniforms[i] = 1;    // 기본 모든 애들 한 개씩
         }
         for (int i : lost) {
-            uniforms[i] -= 1;
+            uniforms[i] -= 1;   // 잃어버린 애들 -1개 처리
         }
         for (int i : reserve) {
-            uniforms[i] += 1;
+            uniforms[i] += 1;   // 여벌있는 애들 +1개 처리
         }
         uniforms[0] = 0;
         uniforms[uniforms.length - 1] = 0;
 
         // 2. 반복하면서 체육복 빌리기
         for (int i = 1; i <= n; i ++) {
-            if (uniforms[i] == 0 && (uniforms[i - 1] == 2 || uniforms[i + 1] == 2)) {
-                uniforms[i]++;
-                if (uniforms[i - 1] == 2) uniforms[i-1]--;
-                else if (uniforms[i + 1] == 2) uniforms[i+1]--;
+            if (uniforms[i] == 0 && (uniforms[i - 1] == 2 || uniforms[i + 1] == 2)) { // if (나 0개 && (앞이 2개 || 뒤가 2개))
+                uniforms[i]++;  // 나 ++;
+                if (uniforms[i - 1] == 2) uniforms[i-1]--;       // if (앞이 2개) 앞--;
+                else if (uniforms[i + 1] == 2) uniforms[i+1]--;  // else if (뒤가 2개) 뒤--;
             }
         }
 
@@ -50,6 +51,7 @@ public class Uniform {
 
     public static void main(String[] args) {
         int output = solution(5, new int[] {1,2,4} , new int[] {1,3,5});
+        System.out.println(output);
         
     }
 }
