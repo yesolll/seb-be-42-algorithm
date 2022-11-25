183ca867acfb442626b299af9c867c3f1ca5a11b
yesolll <9595kys@gmail.com>
2022-11-26 01:42:57 +0900

    Skill Check lv1

---
+++ src/com/yesolll/programmers/myself/challenge/lv2/Biggest.java
@@ -0,0 +1,14 @@
+package com.yesolll.programmers.myself.challenge.lv2;
+
+public class Biggest {
+
+    public static String solution(String number, int k) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("4177252841", 4));
+        // 4자리 제거해서 가장 큰 수 만들기. 순서 변경 x
+    }
+}
