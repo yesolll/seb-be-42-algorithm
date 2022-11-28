b5f6e642d44946ea62665a5fea7ad2ca629cc53b
yesolll <9595kys@gmail.com>
2022-11-28 20:23:31 +0900

    Q77484 로또의 최고 순위와 최저 순위

--- src/com/yesolll/programmers/day7/Lotto.java
+++ src/com/yesolll/programmers/day7/Lotto.java
@@ -3,33 +3,51 @@ package com.yesolll.programmers.day7;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/*
+ * 정답 배열: int[2] answer => [최고등수, 최저등수]
+ * 
+ *                          0  1  2  3  4  5  6
+ * 등수 배열: int[] rank = {6, 6, 5, 4, 3, 2, 1}; index: 맞힌 개수, value: 등수
+ * 
+ * 1. List로 변환 (비교 쉽게 하려고...)
+ * 
+ * ※ 전부 일치하면 return [1, 1]
+ * 
+ * 2. 내 로또번호 순회하면서 0개수, 맞힌 개수 구하기
+ * 
+ * 3-1. 0만 6개면 1등 or 6등
+ * 3-2. 0으로 들어온 게 다 맞았을 때 == 맞힌 개수 + 0 개수
+ * 3-3. 0으로 들어온 게 다 틀렸을 때 == 맞힌 개수
+ */
 public class Lotto {
     public static int[] solution(int[] lottos, int[] win_nums) {
         
-        int[] answer = new int[2];          // 정답 배열: length 2
-        int[] rank = {6, 6, 5, 4, 3, 2, 1}; // 등수 배열: 0개면 6등, 1개면 6등, 2개면 5등 ..., 6개면 1등
+        int[] answer = new int[2];          // 정답 배열 [최고등수, 최저등수]
 
-        List<Integer> sortedLotto = Arrays.stream(lottos).sorted().boxed().collect(Collectors.toList());
-        List<Integer> sortedWin = Arrays.stream(win_nums).sorted().boxed().collect(Collectors.toList());
+        //            0  1  2  3  4  5  6   // index: 맞힌 개수, value: 등수
+        int[] rank = {6, 6, 5, 4, 3, 2, 1}; // 등수 배열 
 
-        if (sortedLotto.containsAll(sortedWin)) return new int[] {1, 1}; // 전부 일치하면 1등
+        List<Integer> lottoNums = Arrays.stream(lottos).boxed().collect(Collectors.toList());
+        List<Integer> winNums = Arrays.stream(win_nums).boxed().collect(Collectors.toList());
+
+        if (lottoNums.containsAll(winNums)) return new int[] {1, 1}; // 전부 일치하면 1등
 
         int zeroCnt = 0;
         int sameCnt = 0;
-        for (int num : sortedLotto) {   
+        for (int num : lottoNums) {         // 내 로또번호 순회하면서
             if (num == 0) {                 // 0 개수 구하기
                 zeroCnt++;
                 continue;
             }
-            if (sortedWin.contains(num)) {  // 맞은 개수 구하기
+            if (winNums.contains(num)) {    // 맞힌 개수 구하기
                 sameCnt++;
             }
         }
+        // 6개 전부 0이면  1등 or 6등
+        if (zeroCnt == 6) return new int[] {1, 6};  
 
-        if (zeroCnt == 6) return new int[] {1, 6};  // 0만 6개면 1등 or 6등
-
-        answer[0] = rank[sameCnt + zeroCnt];    // 0으로 들어온 게 다 맞았을 때 == 최고 등수
-        answer[1] = rank[sameCnt];              // 0으로 들어온 게 다 틀렸을 때 == 최저 등수 (현재 맞은 개수의 등수)
+        answer[0] = rank[sameCnt + zeroCnt];    // 0으로 들어온 게 다 맞았을 때 == 맞힌 개수 + 0 개수 [최고]
+        answer[1] = rank[sameCnt];              // 0으로 들어온 게 다 틀렸을 때 == 맞힌 개수          [최저]
         
         return answer;
     }
