148914a28dd82892493162c5338ae5d97956c336
yesolll <9595kys@gmail.com>
2022-11-28 20:31:53 +0900

    Q77484 로또의 최고 순위와 최저 순위

--- src/com/yesolll/programmers/day7/Lotto.java
+++ src/com/yesolll/programmers/day7/Lotto.java
@@ -1,23 +1,18 @@
 package com.yesolll.programmers.day7;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 /*
  * 정답 배열: int[2] answer => [최고등수, 최저등수]
  * 
  *                          0  1  2  3  4  5  6
  * 등수 배열: int[] rank = {6, 6, 5, 4, 3, 2, 1}; index: 맞힌 개수, value: 등수
  * 
- * 1. List로 변환 (비교 쉽게 하려고...)
- * 
  * ※ 전부 일치하면 return [1, 1]
  * 
- * 2. 내 로또번호 순회하면서 0개수, 맞힌 개수 구하기
+ * 1. 내 로또번호 순회하면서 0개수, 맞힌 개수 구하기
  * 
- * 3-1. 0만 6개면 1등 or 6등
- * 3-2. 0으로 들어온 게 다 맞았을 때 == 맞힌 개수 + 0 개수
- * 3-3. 0으로 들어온 게 다 틀렸을 때 == 맞힌 개수
+ * 2-1. 0만 6개면 1등 or 6등
+ * 2-2. 0으로 들어온 게 다 맞았을 때 == 맞힌 개수 + 0 개수
+ * 2-3. 0으로 들어온 게 다 틀렸을 때 == 맞힌 개수
  */
 public class Lotto {
     public static int[] solution(int[] lottos, int[] win_nums) {
@@ -27,20 +22,17 @@ public class Lotto {
         //            0  1  2  3  4  5  6   // index: 맞힌 개수, value: 등수
         int[] rank = {6, 6, 5, 4, 3, 2, 1}; // 등수 배열 
 
-        List<Integer> lottoNums = Arrays.stream(lottos).boxed().collect(Collectors.toList());
-        List<Integer> winNums = Arrays.stream(win_nums).boxed().collect(Collectors.toList());
-
-        if (lottoNums.containsAll(winNums)) return new int[] {1, 1}; // 전부 일치하면 1등
+        if (lottos.equals(win_nums)) return new int[] {1, 1}; // 전부 일치하면 1등
 
         int zeroCnt = 0;
         int sameCnt = 0;
-        for (int num : lottoNums) {         // 내 로또번호 순회하면서
+        for (int num : lottos) {            // 내 로또번호 순회하면서
             if (num == 0) {                 // 0 개수 구하기
                 zeroCnt++;
                 continue;
             }
-            if (winNums.contains(num)) {    // 맞힌 개수 구하기
-                sameCnt++;
+            for (int i : win_nums) {        
+                if (num == i) sameCnt++;    // 맞힌 개수 구하기
             }
         }
         // 6개 전부 0이면  1등 or 6등
