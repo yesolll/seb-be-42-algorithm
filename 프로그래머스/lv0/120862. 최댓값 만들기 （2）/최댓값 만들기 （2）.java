1677eff2229da372595e3bb8fb08377310c50b70
yesolll <9595kys@gmail.com>
2022-12-01 00:33:53 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.09 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int minusCnt = 0;
+        int minusProduct = 0;
+        int plusProduct = 0;
+        Arrays.sort(numbers);
+        minusCnt = (int) Arrays.stream(numbers).filter(i -> i < 0).count();
+        minusProduct = numbers[0] * numbers[1];
+        plusProduct = numbers[numbers.length-1] * numbers[numbers.length-2];
+        
+        return minusProduct > plusProduct ? minusProduct : plusProduct;
+    }
+}
