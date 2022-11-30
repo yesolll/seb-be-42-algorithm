1ad6d5f8e66a9c831f892024fe5155226a5dadf5
yesolll <9595kys@gmail.com>
2022-11-30 23:39:22 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 2.14 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -4,11 +4,10 @@ class Solution {
         
         for (int i = 0; i < food.length; i++) {
             if (food[i] >= 2)
-            sb.append(String.valueOf(i).repeat(food[i] / 2));
+                sb.append(String.valueOf(i).repeat(food[i] / 2));
         }
 
-        String thisSide = sb.toString();
-        String otherSide = sb.reverse().toString();
-        return thisSide + "0" + otherSide;
+        String thisSide = sb + "0";
+        return thisSide + sb.reverse().toString();
     }
 }
