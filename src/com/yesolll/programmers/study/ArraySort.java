f75403f53726cdb672ca235e23800399d163995a
yesolll <9595kys@gmail.com>
2022-11-30 23:50:12 +0900

    Sorting array

---
+++ src/com/yesolll/programmers/study/ArraySort.java
@@ -0,0 +1,12 @@
+package com.yesolll.programmers.study;
+
+import java.util.Arrays;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,6,5,8,3};
+        Arrays.sort(arr);
+        for(int i : arr) System.out.println(i);
+    }
+    
+}
