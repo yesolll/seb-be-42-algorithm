b95e0b7965a71fe56efe77e5e0cf4410a7ea7361
yesolll <9595kys@gmail.com>
2022-11-30 23:51:49 +0900

    Power

---
+++ src/com/yesolll/programmers/study/Power.java
@@ -0,0 +1,9 @@
+package com.yesolll.programmers.study;
+
+public class Power {
+    public static void main(String[] args) {
+        System.out.println((int) Math.pow(2, 2));
+        System.out.println((int) Math.pow(2, 3));
+        System.out.println((int) Math.pow(2, 4));
+    }
+}
