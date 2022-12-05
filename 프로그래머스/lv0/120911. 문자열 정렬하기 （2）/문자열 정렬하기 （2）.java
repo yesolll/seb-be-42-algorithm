26467cbb5a8138c63bd73335213dcacf88080cf9
yesolll <9595kys@gmail.com>
2022-12-05 22:50:01 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.57 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,14 +1,11 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] arr = new String[my_string.length()];
-        for (int i = 0; i < my_string.length(); i++) {
-            arr[i] = (my_string.charAt(i)+"").toLowerCase();
-        }
-        answer = Arrays.stream(arr).sorted().collect(Collectors.joining());
+        char[] arr = my_string.toLowerCase().toCharArray();
+        Arrays.sort(arr);
+        answer = new String(arr);
         return answer;
     }
 }
