97371c8b5f2321ab023168faa202e0e57d393fe2
yesolll <9595kys@gmail.com>
2022-12-06 21:58:46 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -1,19 +1,6 @@
 class Solution {
     public int solution(int[] array, int n) {
-        int maxDiff = 100;
         int answer = 0;
-        for (int i = 0; i < array.length; i++){
-            int diff = Math.abs(array[i] - n);
-            if(maxDiff > diff){
-                answer = array[i];
-                maxDiff = diff;
-            } else if (maxDiff == diff) {
-                if(answer > array[i]){
-                    answer = array[i];
-                    maxDiff = diff;
-                }
-            }
-        }
         return answer;
     }
 }
