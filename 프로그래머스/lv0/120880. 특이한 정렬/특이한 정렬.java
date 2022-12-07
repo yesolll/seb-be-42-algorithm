16688d0b3455e8f4b46e3613debf62957b8c6f80
yesolll <9595kys@gmail.com>
2022-12-07 22:46:13 +0900

    [level 0] Title: 특이한 정렬, Time: 2.95 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -3,10 +3,32 @@ import java.util.Map.Entry;
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        return Arrays.stream(numlist)
-                .boxed()
-                .sorted((a, b) -> Math.abs(a - n) == Math.abs(b - n) ? b.compareTo(a) : Integer.compare(Math.abs(a - n), Math.abs(b - n)))
-                .mapToInt(Integer::intValue)
-                .toArray();
+        List<Integer> answer = new ArrayList<>();
+        Map<Integer, List<Integer>> numMap = new TreeMap<>();
+        
+        for (int i = 0; i < numlist.length; i++) {
+            List<Integer> values = new ArrayList<>();
+            int diff = Math.abs(numlist[i] - n);
+            if (numMap.get(diff) != null) {
+                values = numMap.get(diff);
+            }
+            values.add(numlist[i]);
+            numMap.put(diff, values);
+        }
+
+        Iterator<Entry<Integer, List<Integer>>> iterator = numMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            List<Integer> value = iterator.next().getValue();
+            Collections.sort(value);
+            if (value.size() == 1) {
+                answer.add(value.get(0));
+            }
+            else if (value.size() == 2) {
+                answer.add(value.get(1));
+                answer.add(value.get(0));
+            }
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
