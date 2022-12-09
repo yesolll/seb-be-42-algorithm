26b7e966e565a70f7c60af8c7d89bed1e7a79dba
yesolll <9595kys@gmail.com>
2022-12-09 22:15:24 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static String solution(int num) {
+        String result = "Odd";
+        
+        if (num % 2 == 0) return "Even";
+        
+        return result;
+    }
+
+}
