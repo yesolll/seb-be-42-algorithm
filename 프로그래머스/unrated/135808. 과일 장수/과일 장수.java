832dca6d94dcae5c7f082df7fb63c17383a5b390
yesolll <9595kys@gmail.com>
2022-12-09 23:00:48 +0900

    [unrated] Title: 과일 장수, Time: 0.45 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -4,11 +4,13 @@ import java.util.stream.Collectors;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        List<Integer> list = Arrays.stream(score).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
-        while (list.size() >= m) {
-            answer += (list.get(m-1) * m);
-            list = list.subList(m, list.size());
+
+        Arrays.sort(score);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
+
         return answer;
     }
 }
