6c92f58d9b1812cc9b41f18cd8118fc2a86b3227
yesolll <9595kys@gmail.com>
2022-12-14 20:13:31 +0900

    [level 0] Title: 최빈값 구하기, Time: 5.69 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Map<Integer, Integer> map = new TreeMap<>();
+        
+        for(int i : array) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        if (map.size() == 1) return array[0];
+
+        List<Entry<Integer, Integer>> sorted =  map.entrySet().stream()
+                                                    .sorted(Collections.reverseOrder(Entry.comparingByValue()))
+                                                    .collect(Collectors.toList());
+        answer = sorted.get(0).getValue() == sorted.get(1).getValue() ? -1 : sorted.get(0).getKey();
+        return answer;
+    }
+}
