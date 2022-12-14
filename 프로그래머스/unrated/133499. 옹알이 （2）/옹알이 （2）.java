8a53fdeb846c8f6540a24cc243c198d43d1384b0
yesolll <9595kys@gmail.com>
2022-12-14 21:48:34 +0900

    [unrated] Title: 옹알이 (2), Time: 4.52 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -2,22 +2,31 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for (String str : babbling) {
-            String word = str.replace("aya","A").replace("ye","B").replace("woo","C").replace("ma","D");
-            word = word.replaceAll("A{2,}+", "x").replaceAll("B{2,}+", "x").replaceAll("C{2,}+", "x").replaceAll("D{2,}+", "x");
-            System.out.println(word);
-            if(isStringUpperCase(word)) answer++;
+            // "aya", "yee", "u", "maa", "yeyemawooye"
+            
+            // 1. 말할 수 있는 단어를 A, B, C, D로 치환
+            String word = str.replace("aya", "A")
+                             .replace("ye",  "B")
+                             .replace("woo", "C")
+                             .replace("ma",  "D");
+            // => "A", "Be", "u", "Da", "BBDCB"
+
+            // 2. 2회 이상 반복된 단어는 X로 치환            
+            word = word.replaceAll("A{2,}+", "X")
+                       .replaceAll("B{2,}+", "X")
+                       .replaceAll("C{2,}+", "X")
+                       .replaceAll("D{2,}+", "X");
+            // "A", "Be", "u", "Da", "XDCB"
+
+            // 결국 말할 수 있는 단어는 [A,B,C,D] 로만 이루어져있다.
+            if(isAvailable(word)) answer++;
         }
         return answer;
     }
-
-    private boolean isStringUpperCase(String str){
-        if (str.length() == 0) return false;
-        char[] charArray = str.toCharArray();
-      
-        for (int i = 0; i < charArray.length; i++) {
-          if (!Character.isUpperCase(charArray[i]))
-            return false;
-        }
-        return true;
+    
+    // 3. [A,B,C,D] 로만 이루어져있으면 true
+    private boolean isAvailable(String str){
+        if(str.replaceAll("[A-D]", "").length() == 0) return true;
+        return false;
     }
 }
