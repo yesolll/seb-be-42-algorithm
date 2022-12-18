21d8d19119333d821ba502d606cae9027bdaed2f
yesolll <9595kys@gmail.com>
2022-12-19 07:28:36 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.72 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -65,11 +65,11 @@ class Solution {
             Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
                 @Override
                 public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                    int comparision = (o1.getValue() - o2.getValue()) * -1;
-                    return comparision == 0 ? o1.getKey().compareTo(o2.getKey()) : comparision;
+                    int comparision = (o1.getValue() - o2.getValue());
+                    return comparision == 0 ? o2.getKey().compareTo(o1.getKey()) : comparision;
                 }
             });
-            answer.append(list.get(0).getKey());
+            answer.append(list.get(1).getKey());
         }
         return answer.toString();
     }
