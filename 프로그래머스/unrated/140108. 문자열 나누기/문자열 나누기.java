a762961d8a4208e57fde24cf071712f9c5f6cdfb
yesolll <9595kys@gmail.com>
2022-12-21 06:05:04 +0900

    [unrated] Title: 문자열 나누기, Time: 0.64 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -1,4 +1,10 @@
 class Solution {
+    // 첫 글자 저장(firstChar)
+    // 반복하면서 firstChar랑 같으면 cnt++ 아니면 --
+    // cnt0이면(개수 똑같으면) answer++
+    // 첫글자를 다음 글자로
+    // 마지막 글자는 비교 안 했으니까 최종적으로 answer++
+        
     public int solution(String s) {
         int answer = 0;
         char firstChar = s.charAt(0);
@@ -7,6 +13,7 @@ class Solution {
         for (int i = 0; i < s.length() - 1; i++) {
             if (firstChar == s.charAt(i)) cnt++;
             else cnt--;
+            
             if ( cnt == 0 ) {
                 answer++;
                 firstChar = s.charAt(i+1);
