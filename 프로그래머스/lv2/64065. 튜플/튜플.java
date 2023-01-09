0ea1f7b8514ea79491600c0c72c258ebb2998c4f
yesolll <9595kys@gmail.com>
2023-01-09 19:06:45 +0900

    [level 2] Title: 튜플, Time: 1.22 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -1,17 +1,20 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String s) {
-        s = s.replaceAll("[^0-9,]", "");
-        String[] arr = s.split(",");
+    public int[] solution(String s) {   // "{{2},{2,1},{2,1,3},{2,1,3,4}}"
+        s = s.replaceAll("[^0-9,]", "");// 2,2,1,2,1,3,2,1,3,4
+        String[] arr = s.split(",");    // [2,2,1,2,1,3,2,1,3,4]
         
         Map<String, Integer> map = new HashMap<>();
         for (String str : arr) {
             map.put(str, map.getOrDefault(str, 0) + 1);
-        }
+        } 
+        // 1=3, 2=4, 3=2, 4=1
         
+        // value 내림차순
         List<Map.Entry<String, Integer>> entryList = new LinkedList<>(map.entrySet());
         entryList.sort(((o1, o2) -> map.get(o2.getKey()) - map.get(o1.getKey())));
+        // 2=4, 1=3, 3=2, 4=1
         
         int[] answer = new int[entryList.size()];
         int idx = 0;
@@ -19,6 +22,7 @@ class Solution {
             answer[idx] = Integer.parseInt(entry.getKey());
             idx++;
         }
+        // [2,1,3,4]
         
         return answer;
     }
